3b2224e35ce7f742cc3a7805835b5ba3f31d511f
Logan Born <logan.o.born@gmail.com>
2020-06-22 15:10:43 -0600

    Update commodity ID to detect vessels and adjectives/modifiers.

--- commodify.py
+++ commodify.py
@@ -13,6 +13,9 @@ from recordclass import make_dataclass
 # Load dictionary for use in making annotations:
 dictionary = np.load( "dict/epsd.npz", allow_pickle=True )["dictionary"].item()
 
+##################################################
+# Sign substitutions:
+
 # TODO Manually emending sign readings will
 # increase accuracy for identification of
 # some commodities. Will require a sound list
@@ -20,13 +23,15 @@ dictionary = np.load( "dict/epsd.npz", allow_pickle=True )["dictionary"].item()
 # from the Nuolenna project?
 # https://github.com/tosaja/Nuolenna/blob/master/sign_list.txt
 # 
-# For now, just map zi3 -> zid2 as proof of concept:
+# For now, just map some manually identified cases as
+# proof of concept:
 sign_substitutions = {
         "zid2":"zi3",
         "kug":"ku3",
         "ku3":"kug",
         "ERIN2":"ERIM",
         }
+
 def substitute( sign ):
     if sign in sign_substitutions:
         yield sign_substitutions[ sign ]
@@ -35,30 +40,43 @@ def substitute( sign ):
         for s in sign_substitutions:
             yield re.sub("(^|-)"+s+"(-|$)","\\1"+sign_substitutions[s]+"\\2",sign)
     return 
+
+# Emend the dictionary:
 words = list(dictionary.keys())
 for word in words:
     for sub in substitute( word ):
         dictionary[ sub ] = dictionary[word]
 
+##################################################
+# Determinative rules:
+
 # Determinatives selected based on ePSD definitions,
 # personal experience, and 
 # https://personal.sron.nl/~jheise/signlists/determin.html
 commodity_determinatives = set([
+        "dug",      # pottery
+        "gan",      # pig 
         "gesz",     # wood
         "gisz",     # wood, alternate spelling
         "gi",       #reed
-        "urudu",    #metal
-        "tug2",     # cloth
+        "gu4",      # cattle
+        "ha",       # tree? Looks like phonetic complement which is 
+                    # only used (in Girsu texts) for {ha}har-ra-na
         "ku6",      # fish
-        #"sza:gan", # TODO pig, or a kind of pot/jar? 
-                    # both seem commodity-like
-        "szagan",   # ^ same
-        "sza",      # ^ same
-        "gan",      # ^ same
-        "sze3",     # wood, cf {sze3}szer7 = {gesz}szer7
-        "zid2",     # grains
-        "zi3",      # grains, alternate name for zid2
+        "kusz",     # leather
+        "muszen",   # bird 
+        "na4",      # stones
         "sar",      # plant
+        "sza:gan",  # pig
+        "szagan",   # pig
+        "sza",      # pig 
+        "sze3",     # wood, cf {sze3}szer7 = {gesz}szer7
+        "tug2",     # cloth
+        "u2",       # plant
+        "uruda",    # metal
+        "urudu",    # metal
+        "zi3",      # grains, alternate name for zid2
+        "zid2",     # grains
         #"ninda",   # Usage seems restricted to {ninda}nindax(DU) 
                     # where it appears to be a length measurement.
         #"munus",   # TODO woman. likely more indicative of a name? 
@@ -66,21 +84,14 @@ commodity_determinatives = set([
         #"da",      # TODO only occurs in banda3{da}, looking like 
                     # a phonetic complement more than a determinative
         #"sza",     # TODO only with (asz){sza}
-        "muszen",   # bird 
         #"gada",    # TODO meaning?
-        "kusz",     # leather
-        "dug",      # pottery
         #"ga2",     # TODO meaning?
         #"an",      # TODO meaning?
-        #"ur3", # TODO only in dur9{ur3} "donkey". Looks again like 
+        #"ur3",     # TODO only in dur9{ur3} "donkey". Looks again like 
                     # phonetic complement. 
-        "ha",       # tree? Looks like phonetic complement which is 
-                    # only used (in Girsu texts) for {ha}har-ra-na
-        "na4",      # stones
-        "gu4",      # cattle
-        "u2",       # plant
         ])
 
+# Mutable alternative to namedtuple:
 Entry = make_dataclass("Entry",
         [
             ("count",dict),
@@ -95,93 +106,138 @@ def new_entry():
     entry.words = []
     return entry
 
-def com_label( words ):
+def label_wordlist( words ):
+    """
+    words:      a list of words to label as commodities
+    returns:    the input list, with commodities labeled
+                by _COM, modifiers such as adjectives labeled
+                by _MOD, and vessels labeled as _VES
+    """
     if words == []:
         return words 
-    # don't label dates:
+
+    # Don't label dates:
     if words[0] in set(["mu", "u4", "iti"]):
         return words
-    # this is a total : remove to avoid double-counts?
-    if words[0] in set(["szunigin", "gu2-an-sze3", "szu-nigin2"]):
+
+    # If this line is a total, we should ignore it to avoid double counts
+    # "dur" can also mark a total, or be a counted object
+    if words[0] in set(["szunigin", "gu2-an-sze3", "szu-nigin2"]): 
         words += ["TOTAL"]
-    # donors and explanatory information:
+    # Donors and explanatory information:
     #if words[0] in set(["ki", "giri3"]):
         #return words
+    # If there is no count we assume (perhaps incorrectly)
+    # that nothing is being counted in this line:
     if "###" not in words:
-        # ?
         return words
 
-
+    # Now we annotate words with various features
+    # We will use these features to manually decide
+    # what is a commodity, but moving forward we can
+    # add more features and train a real ML model:
     features = [[] for _ in words]
     for i,word in enumerate(words):
+
+        FEAT_NONE = 0
+        FEAT_COM  = 1
+        FEAT_PERS = 2
+        FEAT_ADJ  = 3
+        FEAT_VES  = 4
 
         # DETERMINATIVES
         if any( "{%s}"%(det) in word for det in commodity_determinatives ):
             #or any(defn[1] == "NN" for defn in dictionary[word]) \
-            features[i].append( 1 )
+            features[i].append( FEAT_COM )
         else:
-            features[i].append( 0 )
+            features[i].append( FEAT_NONE )
 
-        features[i].append(0)
-        FEAT_COM = 1
-        FEAT_PERS = 2
-        FEAT_ADJ = 3
+        # SYNSETS
+        features[i].append( FEAT_NONE )
         if word in dictionary:
+            # Check all definions of the word until one of them
+            # gives us evidence to make a decision:
             for defn, POS in dictionary[word]:
                 defn = defn.replace("?","")
+                # Does this definition represent a commodity?
                 is_com, evidence = semantic.is_commodity_synset( 
                         word, 
-                        semantic.get_hypernyms( defn )
+                        semantic.get_noun_hypernyms( defn )
                     )
                 if is_com:
-                    features[i][-1] = FEAT_COM
+                    if any( "vessel" in str(synset) for synset in evidence):
+                        features[i][-1] = FEAT_VES
+                    else:
+                        features[i][-1] = FEAT_COM
                     break
                 elif not is_com and evidence != []:
                     if evidence == "person":
                         features[i][-1] = FEAT_PERS
-                    elif evidence == "adj":
+                    elif evidence == "vessel":
+                        features[i][-1] = FEAT_VES
+                    elif evidence == "mod":
                         features[i][-1] = FEAT_ADJ
                     elif isinstance( evidence, list ) and type( evidence[0] ).__name__ == "Synset":
                         if any( "person" in str(synset) for synset in evidence ):
                             features[i][-1] = FEAT_PERS
                         else:
                             features[i][-1] = -1
-                        #if any( "person." in str(e) for e in evidence ):
-                            # implicit ration?
                     else:
                         features[i][-1] = -1
                     break
         else:
+            # If word is not in the dictionary, don't pass a list of synsets
             is_com, evidence = semantic.is_commodity_synset( word, None )
             if is_com:
                 features[i][-1] = FEAT_COM
 
 
 
+    # Use the features from the last section to 
+    # label the words in the input list:
     maybe_ration = False
+    # If two commodity-like items occur within this many words
+    # of one another, the second will be labeled as a modifier.
+    # This helps with cases like siki udu, where udu and siki
+    # can both be commodities but udu here acts as a modifier.
+    mod_proximity_threshold = 3
     for i in range(len(words)):
-        #if not "###" in words[max(0,i-3):i]:
-            #continue
-        if features[i][0] == 1:
-            if i>0 and any( w.endswith("_COM") for w in words[max(0,i-3):i]):
-                pass
-            else:
-                words[i] += "_COM"
-        elif features[i][1] == FEAT_COM:
+        context = words[max(0,i-mod_proximity_threshold):i]
+        if features[i][0] == FEAT_COM:
             # This check helps avoid labeling modifiers
             # as commodities: e.g. in zi3 sig15, only
             # label zi3, as we would in e.g. ku6 dar-ra
-            if i>0 and any( w.endswith("_COM") for w in words[max(0,i-3):i]):
-                pass
+            if i>0 and any( "_" in w for w in context ):
+                words[i] += "_MOD"
+            else:
+                words[i] += "_COM"
+        elif features[i][1] == FEAT_VES:
+            words[i] += "_VES"
+        elif features[i][1] == FEAT_COM:
+            if i>0 and any( "_" in w for w in context):
+                words[i] += "_MOD"
             else:
                 words[i] += "_COM"
         elif features[i][1] == FEAT_ADJ:
-            # $mun$
-            if len(words) == 1:
-                words[i] += "_COM"
+            # If this entry only contains one word and that word looks
+            # like an adjective/modifier, we instead count it as a 
+            # commodity: helps with e.g. mun "salt" which usually modifies
+            # a fish (ku6 mun) but can also be counted on its own.
+            if len([w for w in words if w != '###']) == 1 or all([features[i][1]==FEAT_ADJ for i in range(len(words)) if words[i]!='###']):
+                if i>0 and any( "_" in w for w in context):
+                    words[i] += "_MOD"
+                else:
+                    words[i] += "_COM"
+            else:
+                words[i] += "_MOD"
         elif features[i][1] == FEAT_PERS:
-            #if len(words) == 1:
+            # If this is a person, we infer that 
+            # there may be an implicit ration:
             maybe_ration = True
+    # If there is a commodity in the entry, then
+    # the person is probably an owner or recipient.
+    # If there is no commodity, then there is probably
+    # an implicit ration:
     if maybe_ration:
         if not any( "_COM" in w for w in words ):
             words.append( "implied_ration?" )
@@ -220,45 +276,23 @@ def commodify( text ):
 
         for string, counts in entry_:
             if counts is not None:
-                #entry.words = com_label( entry.words )
+                #entry.words = label_wordlist( entry.words )
                 #entries.append( entry )
                 #entry = new_entry()
                 entry.count = {"string":string, "readings":counts}
                 entry.words.append( "###" )
             else:
                 entry.words.append( string )
-        entry.words = com_label( entry.words )
+        entry.words = label_wordlist( entry.words )
         entries.append( entry )
 
-        """
-        dist_from_numeral += 1
-
-        if word == "x" or word == "...":
-            entry.words += [word]
-            #print( word, end=' ' )
-            continue
-
-        elif counts is not None and len(counts) > 0:
-            # This is a numeral (boundary between items)
-            entry.words = com_label( entry.words )
-            entries.append( entry )
-            entry = new_entry()
-            entry.count = {"string":word,"readings":counts}
-            dist_from_numeral = 0
-            found_com = False
-            continue
-
-        else:
-            entry.words += [word]
-        """
-        
     # Tag and append the final entry:
-    #entry.words = com_label( entry.words )
+    #entry.words = label_wordlist( entry.words )
     #entries.append( entry )
     entries = [ entry for entry in entries if not (entry.count is None and entry.words == [])]
     return entries
 
-if __name__ == "__main__":
+def commodify_whole_corpus():
     # *_COM -> num string -> number of occurrences
     counts_by_commodity = defaultdict(lambda:defaultdict(int))
     # *_COM -> [values]
@@ -267,15 +301,16 @@ if __name__ == "__main__":
     collocation_counts = defaultdict(int)
 
     commodified_texts = []
-    #for text in data.girsu[:100]: # FIXME
-    for text in data.girsu_lined[:100]: # FIXME
+    for text in data.girsu_lined:
         commodified_texts.append( commodify( text ) )
 
-    """
     for entries in commodified_texts:
         if len(entries) > 5 and len(entries) < 50:
             for entry in entries:
                 
+                if entry.words.count("###") > 1:
+                    continue
+
                 if entry.count is not None:
                     count = entry.count["string"]
                     values = entry.count["readings"]
@@ -323,23 +358,12 @@ if __name__ == "__main__":
             "values_by_commodity": dict(values_by_commodity),
             "all_objects": list(all_objects),
         }
-    #print(output_json)
-    """
-    #for i in range(len(commodified_texts)):
-        #if data.lined_names[i] in [
-                #"P142827",
-                ##"P142850",
-                #"P249089",
-                #"P100023",
-                #"P459081",
-                #"P459082",
-                #]:
-            #text = commodified_texts[i]
-    for text in commodified_texts:
-            for entry in text:
-                print(entry.words)
-            print()
-            print("="*50)
-            print()
 
-    # TODO Some refinements needed: is {gesz}RU the same as {gesz}RU-ur-ka minus morphology?
+    return output_json
+
+if __name__ == "__main__":
+    output_json = commodify_whole_corpus()
+
+    outfile = open("commodities.json", "w")
+    json.dump( output_json, outfile )
+    outfile.close()
--- semantic.py
+++ semantic.py
@@ -1,6 +1,4 @@
 #!/bin/python
-# Usage: ./wordlist.sh | python analyse.py
-
 from nltk.corpus import wordnet as wn
 
 import json
@@ -16,9 +14,14 @@ from recordclass import make_dataclass
 # Load dictionary for use in making annotations:
 dictionary = np.load( "dict/epsd.npz", allow_pickle=True )["dictionary"].item()
 
-def get_hypernyms( word ):
+def get_noun_hypernyms( word ):
+    """
+    word:       An English word
+    returns:    A set of Synset objects representing the given
+                word's hypernyms. Only returns noun synsets.
+    """
     all_hypernyms = wn.synsets( word )
-    # limit to noun readings:
+    # Limit to noun readings:
     all_hypernyms = [h for h in all_hypernyms if ".n." in str(h)]
     i = 0
     while i < len(all_hypernyms):
@@ -28,100 +31,148 @@ def get_hypernyms( word ):
     all_hypernyms = set(all_hypernyms)
     return all_hypernyms
 
+
 def is_commodity_synset( word, synsets ):
+    """
+    word:       A Sumerian word
+    synsets:    A set of Synsets representing the possible readings
+                for this word.
+    returns:    (is_com, evidence) A tuple where the first item is a boolean
+                specifying whether the item is probably a commodity, and the
+                second item is a summary of the evidence used to arrive at this
+                conclusion. evidence can be a string specifying what kind of
+                object the input represents ("mod", "vessel", "person") or a 
+                list of synsets.
+    """
+    # List of words that are used as units, and should not be labeled as commodities:
     units = [
             "asz", "ban2", "barig", "bur3", "danna", "disz",
             "dug", 
-            #"esz2", # flour, rope
             "esze3", "gan2", "gesz2", "geszu",
-            #"gi", 
             "gin2", "gu2", "gur", "guru7", "iku", "kusz3",
-            #"ninda", 
             "sar", "sila3", "szar2", "szargal", 
-            #"sze",
             "u", "usz",
-            "la2", # subtraction - not a unit but part of the number #
-            # TODO kur2 Unit amount?
-            # TODO kusz3 Unit amount?
-            "{ninda}nindax(DU)", # apparently another spelling for ninda
+            "la2", # subtraction - not a unit but part of the number
+            "{ninda}nindax(DU)", # another spelling for ninda
+            #"ninda", # This can also represent bread; unlike the other
+            # spelling this one is ambiguous
+            #"sze", # barley, can be a counted object
+            #"esz2", # flour, rope, can be counted
+            #"gi",# reeds, can be counted
+            "gu-la2", # sheaves
+            "kusz3", # unit of length
+            "kur2", # vessel size
         ]
+    # List of words which may look like commodities based on their synsets,
+    # but are not. The value in the dictionary should represent why they
+    # aren't commodities: 
+    #   mod: for adjectives and modifiers like mun ("salted"). Usually preceeded
+    #        by a commodity.
+    #   person: for words which represent people but may look like animals
+    #           based on their synsets. Often implies a ration amount
+    #   vessel: for words which represent containers or other aggregations
+    #           of items. Usually followed by a commodity.
+    #   "": for words which are not commodities, and carry no special information
+    #       that can be used to help identify nearby words
+    #   date: for words used in writing dates and durations
     blacklist_words = {
-            "ba":"",# allot, precedes a name, usually after a field area
-            "murgu2":"adj",# shoulder/spine of fruit
-            "a2":"",#"half-day's labor"
-            "lugal":"person", # king
-            "geme2":"person", # worker (human, not animal)
-            "sa":"bundle", # bundle TODO this implies the following word is a commodity, often eg turnips
-            # TODO dag "pot" also implies following is commodity, but sometimes occurs on its own
-            # TODO sa2-du11 "offering" implies following is com
-            # TODO nig2-du3-a "string" of fruit, following word
-            # todo munus-me person - ration?
-            "ge6":"adj", # black - missing from dictionary
-            "x":"",
-            "babbar2":"adj", # white
-            "sag":"", # TODO many meanings - some disambiguation? #
-            "dar-ra":"adj", # split - adjective modifier
-            "za-na":"", # TODO meaning?
-            "gu-la2":"adj", # large/gal TODO a unit? word order is wrong for it to be "large garlic" in gu-la2 szum2 szuh5-ha
-            "u4":"date", # days - these are the object being counted but are they relevant?
-            "mu":"date",#year
-            "mun":"adj", # TODO blacklist only when no other object
-            "dub-sar":"person", # scribe, the person not the implement
-            "ba-ug7-ge":"",
+            "x":"", # broken sign, do not count as an object
+
+            "a2":"", # "half-day's labor"
+            "a-ra2":"", # times, as in a-ra2 1(disz)-kam the first time
+            "ba":"", # "allot", precedes a name, usually after a field area
+            "ba-ug7-ge":"", # die, the verb, not the noun
+            "giri3":"", # through, from, via
+            "ma2":"", # boat
+            "ma2-gur8":"", # boat
+            "sag":"", # TODO has many meanings, some WSD might be helpful
             "si-i3-tum":"", #remainder
-            "a-ra2":"", #times, as in a-ra2 1(disz)-kam the first time
-            "na":"adj", #stone, used for denoting means of measurement
-            "li":"adj",#oil, as in udu nita li
-            "nim":"adj",#fly?
-            "nita":"adj",#male, animal
-            "nita-me":"person",#male, person?
-            "giri3":"",#through, from, via
-            "nu-siki":"adj",
-            "igi-nu-du8":"person", # blind 
             "us2-sa":"",
+
+            "dub-sar":"person", # scribe, the person not the implement
+            "geme2":"person", # worker (human, not animal)
+            "igi-nu-du8":"person", # blind 
+            "lugal":"person", # king
+            "nita-me":"person",#male, person?
+            
+            "dag":"vessel", # pot
+            "nig2-du3-a":"vessel", # "string" of fruit, following word is a commodity
+            "sa":"vessel", # bundle, implies the following word is a commodity, often eg turnips
+            
+            "babbar2":"mod", # white
+            "dar-ra":"mod", # split - adjective modifier
+            "gi":"mod", # reed - sometimes a unit, but counted in contexts like sa gi
+            "ge6":"mod", # black - missing from dictionary
+            "li":"mod", # oil, as in udu nita li
+            "mun":"mod", # salt
+            "murgu2":"mod", # shoulder/spine of fruit
+            "na":"mod", # stone, used for denoting means of measurement
+            "nim":"mod", # fly?
+            "nita":"mod", # male, animal
+            "nu-siki":"mod", # orphan, usually describes animals
+
+            "mu":"date", # year
+            "u4":"date", # days
+
+            # TODO sa2-du11 "offering" implies following is commodity, but it is not a vessel
+            # do we change the term "vessel" to something more generic?
+            # TODO munus-me person - implies a ration? likewise nita-me and en-ku3
         }
+    # Words which would not get labeled based on their synsets, but which
+    # should be counted as commodities. Also useful for words which are
+    # not in the dictionary:
     whitelist_words = [
-            "kas", # often written in place of kasz "beer"
-            #"dam", # TODO spouse, but sometimes looks like a female slave?
-            "gu4", # cattle
-            # nu-siki orphan - orphaned animals or people?
-            # nita-me male - person?
-            # en-ku3 - person - implied ration?
-            "tug2",
+            "am-ra", # beam, for a boat
             "ga'ar", # cheese
-            "szuku", # ration
-            "ud5", # goat
+            "gu4", # cattle
+            "kas", # often written in place of kasz "beer"
+            #"dam", # TODO spouse, but sometimes looks like a female worker being counted. 
+            "sze",
             "SZIM", # aromatic/perfume
             "szim",
-            "sze",
+            "szuku", # ration
+            "tug2",
+            "ud5", # goat
         ]
+    # Words with any of these values in their synsets will not be counted as commodities:
     blacklist_syns = [
-            "person."
+            "person.",
+            "reservoir.",
         ]
+    # Words with any of these values in their synsets will be counted as commodities:
     whitelist_syns = [
             "implement.", 
+            'tool.', 
+            'musical_instrument.', 
+
             'food.', 
             'plant.', 
+
             'material.', 
-            'animal.', 
-            'tool.', 
+            'clothing.', 
+
             "metal", 
             "stone.", 
             "rock.", 
-            'tree.', 
-            'commodity.', 
-            'clothing.', 
+
+            'animal.', 
             'skin.', 
             'hair.', 
-            'musical_instrument.', 
+
+            'tree.', 
             'wood.',
-            'vessel.'
+
+            'vessel.',
+            'container.',
+
+            'commodity.', 
         ]
+
     if word in units:
         return False, ["UNIT"]
 
     if word in blacklist_words:
-        return False, blacklist_words[word] #["BLACKLIST"]
+        return False, blacklist_words[word]
 
     if word in whitelist_words:
         return True, ["WHITELIST"]
@@ -129,14 +180,15 @@ def is_commodity_synset( word, synsets ):
     if synsets is None:
         return False, []
 
+    # Check for matches in the synsets:
     matches = [ synset for synset in synsets if any(term in str(synset) for term in whitelist_syns) ]
     bad_matches = [ synset for synset in synsets if any(term in str(synset) for term in blacklist_syns) ]
 
-    #if word == "ku3-bi":
-        #print(word,synsets, matches, bad_matches)
-    
     if matches != []:
         return True, matches
     if bad_matches != []:
         return False, bad_matches
+    # If we have no rules that apply to this word, do not label 
+    # it as a commodity, but return an empty evidence list so 
+    # the caller knows the term is not blacklisted:
     return False, []
